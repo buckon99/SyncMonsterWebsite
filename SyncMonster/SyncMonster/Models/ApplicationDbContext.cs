3da564ead8d773b14d09cdf5b86c51fafef8ebc9
Jack Buckon <jackbuckon@gmail.com>
2016-08-17 01:15:33 -0400

    updated API config

---
+++ SyncMonster/SyncMonster/Models/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SyncMonster.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+
+        public ApplicationDbContext() : base("DefaultConnection", throwIfV1Schema: false)
+        {
+            Database.SetInitializer<ApplicationDbContext>(new CreateDatabaseIfNotExists<ApplicationDbContext>());
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
